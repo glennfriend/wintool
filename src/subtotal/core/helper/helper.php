b3e5bd9d82e5d982350f0ba3e77dca341806c374
training4 <glenn.profile@gmail.com>
2017-03-10 00:14:59 -0800

    update core

--- src/subtotal/core/helper/helper.php
+++ src/subtotal/core/helper/helper.php
@@ -1,6 +1,47 @@
 <?php
+use App\Utility\Log;
 
-function hello()
+/**
+ *
+ */
+function isCli()
 {
-    echo 'helo!!!!!!!!!!!!!!!!!!!!!!!!!!!!!';
+    return PHP_SAPI === 'cli';
+}
+
+/**
+ *
+ */
+function conf($key)
+{
+    return Config::get($key);
+}
+
+/**
+ *
+ */
+function pr($data, $writeLog=false)
+{
+    echo '<pre style="background-color:#def;color:#000;text-align:left;font-size:10px;font-family:Hack,dina,GulimChe;">';
+    if (is_object($data) || is_array($data)) {
+        print_r($data);
+    }
+    else {
+        echo $data;
+        echo PHP_EOL;
+    }
+    echo "</pre>" . PHP_EOL;
+
+    if (!$writeLog) {
+        return;
+    }
+
+    if (is_object($data) || is_array($data)) {
+        Log::record(
+            print_r($data, true)
+        );
+    }
+    else {
+        Log::record($data);
+    }
 }
