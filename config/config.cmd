9f336e7758d7891df735f89b43835259307fcae5
training4 <glenn.profile@gmail.com>
2017-03-15 20:40:56 -0700

    update

--- config/config.cmd
+++ config/config.cmd
@@ -1,9 +1,8 @@
 :: 
 :: 在您安裝好 xampp 之後
-:: 請在這裡指定 xampp 的路徑
+:: 請在這裡指定該的路徑
 :: 
-:: 	example:
-:: 		c:\xampp
+::  example:
+::      c:\xampp
 :: 
-@SET MY_XAMPP_PATH=D:\xampp
-
+@SET MY_XAMPP_PATH=C:\xampp
